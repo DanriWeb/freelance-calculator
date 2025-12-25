ecbce34b7b2368aed344d8085ca10e5a103236d8
DanriWeb <danri.web@gmail.com>
2025-12-25 10:39:22 +0500

    feat(ui): :lipstick: реализовать компонент Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header__title-container">
+        <h1 className="header__title">MVP Freelance Calculator</h1>
+      </div>
+    </header>
+  );
+}
